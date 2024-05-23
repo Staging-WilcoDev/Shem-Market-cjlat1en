d15a8fe46f3bba02dd945941d3ae752c8241cbdf
WilcoApp <hello@wilco.gg>
2024-05-23 14:55:37 +0300

    Initial commit

--- backend/main.go
+++ backend/main.go
@@ -7,44 +7,18 @@ import (
 
 func main() {
 	router := gin.Default()
-	router.GET("/items", getItems)
+	router.GET("/", greet)
 	router.HEAD("/healthcheck", healthcheck)
-	router.POST("/item", addItem)
 
 	router.Run()
+}
+
+func greet(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, "Welcome, Go navigator, to the Anythink cosmic catalog.")
 }
 
 func healthcheck(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"status": "ok",
 	})
-}
-
-// create a variable that store those itemm name:  'Galactic Goggles', 'Meteor Muffins', 'Alien Antenna Kit', 'Starlight Lantern', and 'Quantum Quill' and an id
-var items = []struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}{
-	{1, "Galactic Goggles"},
-	{2, "Meteor Muffins"},
-	{3, "Alien Antenna Kit"},
-	{4, "Starlight Lantern"},
-}
-
-func getItems(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, items)
-}
-
-func addItem(c *gin.Context) {
-    var item struct {
-        ID   int    `json:"id"`
-        Name string `json:"name"`
-    }
-    if err := c.BindJSON(&item); err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-        return
-    }
-    item.ID = len(items) + 1
-    items = append(items, item)
-    c.JSON(http.StatusCreated, item)
 }
